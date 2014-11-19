9773935f3729ea4732742177bd12c2e8a953ad8c
Travis <travis@haleycomputersolutions.com>
2014-11-19 14:45:26 -0700

    birth

---
+++ gitdeploytest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gitdeploytest.MvcApplication" Language="C#" %>
---
+++ gitdeploytest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitdeploytest.Startup))]
+namespace gitdeploytest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
